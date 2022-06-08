1d414935505b4ae28b1254ae347687176a91975e
AlanGacihi <alanmamicha@gmail.com>
2022-06-08 08:35:42 -0700

    Fix 4

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if not a_dictionary:
+    if a_dictionary is None:
         a_dictionary = {key: value}
         return a_dictionary
     a_dictionary[key] = value
