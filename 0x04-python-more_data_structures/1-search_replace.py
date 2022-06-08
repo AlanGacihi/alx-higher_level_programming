883659881f9655b7cefb5bd3e0cffce7aba4da3b
AlanGacihi <alanmamicha@gmail.com>
2022-06-08 08:29:54 -0700

    Fix 2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
+    if my_list is None:
         return None
     return [i if i != search else replace for i in my_list]
