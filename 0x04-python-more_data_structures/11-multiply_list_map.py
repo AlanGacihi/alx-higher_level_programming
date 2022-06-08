f7093dcd7f544a4152dd21d9f6286c74a5e3d632
AlanGacihi <alanmamicha@gmail.com>
2022-06-08 08:37:56 -0700

    Fix 5

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    return None if not my_list else list(map((number).__mul__, my_list))
+    return None if my_list is None else list(map((number).__mul__, my_list))
