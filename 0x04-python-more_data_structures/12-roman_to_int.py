4df7de96e978485fbb9d34e6e11e256388c0f087
AlanGacihi <alanmamicha@gmail.com>
2022-06-12 08:06:36 -0700

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return None
+
+    conv = {"I": 1, "V": 5, "X": 10,
+            "L": 50, "C": 100, "D": 500,
+            "M": 1000}
+    n = 0
+    l = len(roman_string)
+    i = 0
+
+    while(i < l):
+        if roman_string[i] == "I" and i != l - 1:
+            if roman_string[i + 1] != "I":
+                n += conv[roman_string[i + 1]] - 1
+                i += 2
+            else:
+                n += conv[roman_string[i]]
+                i += 1
+        else:
+            n += conv[roman_string[i]]
+            i += 1
+
+    return n
