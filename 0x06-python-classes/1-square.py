93383888ebca91fb2de034a2680e0329839bd5d6
AlanGacihi <alanmamicha@gmail.com>
2022-06-21 21:22:59 -0700

    Add docstring

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+
+"""Square"""
+
+
 class Square:
+    """"Define a square"""
+
     def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+        size (int): The size of the new square.
+        """
         self.__size = size
