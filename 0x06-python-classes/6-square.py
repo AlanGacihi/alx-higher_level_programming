310d52fcadf6b74797a41ac557cee1ef75ea2865
AlanGacihi <alanmamicha@gmail.com>
2022-06-21 22:11:55 -0700

    Add doc

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,7 +3,13 @@
 """Square"""
 
 class Square:
+    """Define a square"""
+
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
+        Args:
+        size (int): The size of the new square.
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
