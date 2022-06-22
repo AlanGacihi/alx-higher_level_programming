5a7b6793139efaac1f497089482d5455a597980c
AlanGacihi <alanmamicha@gmail.com>
2022-06-21 22:09:14 -0700

    Add doc

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+x#!/usr/bin/python3
 
 """Square"""
 
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
+"""Square"""
+
 class Square:
     def __init__(self, size=0, position=(0, 0)):
         if type(size) is not int:
