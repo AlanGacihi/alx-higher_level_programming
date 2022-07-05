1685457f151f85e39d10442c21d1df1e9823f5ce
AlanGacihi <alanmamicha@gmail.com>
2022-07-05 08:22:47 -0700

    Fix

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -17,8 +17,24 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
         """
         Returns the dictionary representation of a class.
         """
-        return self.__dict__
+        if type(attrs) is not list:
+            return self.__dict__
+        ans = dict()
+        for elem in attrs:
+            if type(elem) is not str:
+                return self.__dict__
+            if elem in self.__dict__.keys():
+                ans[elem] = self.__dict__[elem]
+        return ans
+
+    def reload_from_json(self, json):
+        """
+        Function that replaces the attributes on a class from a json.
+        """
+        for key, val in json.items():
+            if key in self.__dict__.keys():
+                self.__dict__[key] = val
