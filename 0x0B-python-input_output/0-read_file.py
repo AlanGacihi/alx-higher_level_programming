28c77bf31dc9271d97713f5b26164212c63b7ea6
AlanGacihi <alanmamicha@gmail.com>
2022-07-05 08:08:44 -0700

    Fix pycodestyle

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     Function that reads the contents of a file.
     """
     with open(filename, 'r', encoding='utf-8') as f:
-            print(f.read(), end="")
+        print(f.read(), end="")
