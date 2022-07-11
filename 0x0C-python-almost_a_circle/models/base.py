1e8af369d8114a718b23591da364f57179d14fa7
AlanGacihi <alanmamicha@gmail.com>
2022-07-11 06:56:45 -0700

    Add base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,21 @@
-class
+#!/usr/bin/python3
+"""
+Contains the class Base
+"""
+
+
+class Base:
+    """
+    The base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialization of the class
+        """
+        if id is not None:
+            self.id = id
+        else:
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
