cfa111faa272eb90c91dc400cf04e8c1f2245144
AlanGacihi <alanmamicha@gmail.com>
2022-08-01 05:06:56 -0700

    Fix indentation

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -17,8 +17,8 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            return self.__size ** 2
+    def area(self):
+        return self.__size ** 2
 
     @property
     def size(self):
