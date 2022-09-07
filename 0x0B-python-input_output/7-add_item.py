cf40b25d595b4c59bd70e3559fbc3e353132b8fd
AlanGacihi <alanmamicha@gmail.com>
2022-09-07 03:10:32 -0700

    Fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,8 @@ import os.path
 if not os.path.exists("add_item.json"):
     with open("add_item.json", "w", encoding='utf-8') as f:
         f.write("[]")
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 my_list = load_from_json_file("add_item.json")
 argc = len(argv)
 for i in range(1, argc):
