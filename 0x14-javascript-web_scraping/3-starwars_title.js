305fd5f68d28ac3c75b7aae0d5f9b1311debb023
AlanGacihi <alanmamicha@gmail.com>
2023-01-20 00:43:33 -0800

    Fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-request('https://swapi.co/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) console.log(err);
-  else console.log(JSON.parse(body).title);
+const end = process.argv[2];
+
+request('https://swapi-api.hbtn.io/api/films/' + end, function (error, respones, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
 });
